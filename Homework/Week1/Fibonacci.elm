7d3176dc11f1338a63709ddfe9bfd0f91fbd1c6f
Aric Parkinson <aparkinson@hirevue.com>
2017-06-06 12:42:26 -0600

    Intitial commit, and week one homework

---
+++ Homework/Week1/Fibonacci.elm
@@ -0,0 +1,47 @@
+module Fibonacci exposing (..)
+
+fibonacci : Int -> Int
+fibonacci n = 0
+
+test : Bool
+test = test1
+    && test2
+    && test3
+    && test4
+    && test5
+    && test6
+    && test7
+    && test8
+    && test9
+    && test10
+
+test1 : Bool
+test1 = factorial 1 == 1
+
+test2 : Bool
+test2 = factorial 2 == 1
+
+test3 : Bool
+test3 = factorial 3 == 2
+
+test4 : Bool
+test4 = factorial 4 == 3
+
+test5 : Bool
+test5 = factorial 5 == 5
+
+test6 : Bool
+test6 = factorial 6 == 8
+
+test7 : Bool
+test7 = factorial 7 == 13
+
+test8 : Bool
+test8 = factorial 8 == 21
+
+test9 : Bool
+test9 = factorial 9 == 34
+
+test10 : Bool
+test10 = factorial 10 == 55
+
---
+++ Homework/Week1/Sort.elm
@@ -0,0 +1,25 @@
+module Sort exposing (..)
+
+sort : [Int] -> [Int]
+sort list = list
+
+test = testEmptyList
+    && testSingletonList
+    && testReverseSortedList
+    && testSortedList
+    && testScrambledList
+
+testEmptyList : Bool
+testEmptyList = sort [] == []
+
+testSingletonList : Bool
+testSingletonList = sort [1] == [1]
+
+testReverseSortedList : Bool
+testReverseSortedList = sort [5,4,3,2,1] == [1,2,3,4,5]
+
+testSortedList : Bool
+testSortedList = sort [1,2,3,4,5] == [1,2,3,4,5]
+
+testScrambledList : Bool
+testScrambledList = sort [4,2,3,1,5] == [1,2,3,4,5]
