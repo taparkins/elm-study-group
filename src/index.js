ba4f993e86ed6b1e0bf39710401a35d7d1ffab8f
Aric Parkinson <aparkinson@hirevue.com>
2017-07-10 21:56:22 -0600

    Adding routing for future week assignments

--- src/index.js
+++ src/index.js
@@ -19,6 +19,21 @@ var router = function() {
     case 'week4':
         week4Module.Homework.Week4.Main.embed(mountNode);
         break;
+    case 'week4/solution':
+        week4Module.Homework.Week4.Solution.Main.embed(mountNode);
+        break;
+    case 'week5':
+        week5Module.Homework.Week5.Main.embed(mountNode);
+        break;
+    case 'week5/solution':
+        week5Module.Homework.Week5.Solution.Main.embed(mountNode);
+        break;
+    case 'week6':
+        week6Module.Homework.Week6.Main.embed(mountNode);
+        break;
+    case 'week6/solution':
+        week6Module.Homework.Week6.Solution.Main.embed(mountNode);
+        break;
     default:
         mainModule.Src.Main.embed(mountNode);
         break;
