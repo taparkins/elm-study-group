1ba27d94e52c82dd95a3d0cb56e75e01e4322ff1
Aric Parkinson <aparkinson@hirevue.com>
2017-07-16 14:30:34 -0600

    Adding comment to Week4 homework so people who weren't present know what the objective was.

--- Homework/Week4/Main.elm
+++ Homework/Week4/Main.elm
@@ -2,6 +2,15 @@ module Homework.Week4.Main exposing (..)
 
 import Html
 
+
+-- Week 4 Homework: Display a Sudoku Board
+--
+-- The goal isn't necessarily to make this pretty, but just to print out a
+-- board that we can play with in future weeks. For now, the board JUST has
+-- to display a static sample. We'll be playing around with changing the
+-- content next week.
+
+
 type alias Model = {}
 
 initModel : Model
